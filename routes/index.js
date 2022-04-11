8a4f9501504e4dea06bc2b43610c6d80b46131af
Gregory Olive <gregoryolive@outlook.com>
2022-04-12 00:12:47 +0900

    Add date-fns to display dates correctly and prepare for heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const fns = require('date-fns');
+const formatDate = (date) => {
+  return fns.formatDistance(
+    date,
+    new Date(),
+    { addSuffix: true }
+  );
+};
+
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
+    text: "Do a barrel roll!",
+    user: "Star Fox",
+    added: new Date('March 02, 2022 03:24:00'),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
+    text: "No gods or kings. Only man.",
+    user: "Andrew Ryan",
+    added: new Date('December 17, 2021 03:24:00'),
   }
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+  res.render('index', { title: 'Mini Messageboard', messages: messages, formatDate: formatDate });
 });
 
 /* GET new message form. */
@@ -28,8 +37,8 @@ router.get('/new', function(req, res, next) {
 router.post('/new', function(req, res, next) {
   const messageText = req.body.message;
   const messageUser = req.body.name;
-  messages.push({text: messageText, user: messageUser, added: new Date()});
-  res.redirect('/')
+  messages.unshift({text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
